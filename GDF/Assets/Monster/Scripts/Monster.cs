e6e3822459f2d3c4d00bdb4046d3f95691819230
Tyler C <TylerCode@users.noreply.github.com>
2019-11-15 15:57:59 -0700

    Monster script up

---
+++ GDF/Assets/Monster/Scripts/Monster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    private GameObject _player;
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_agent.isStopped == true)
+        {
+            _agent.isStopped = false;
+            _agent.destination = _player.transform.position;
+        }
+
+        _agent.destination = _player.transform.position;
+    }
+}
