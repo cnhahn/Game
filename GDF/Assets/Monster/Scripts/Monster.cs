2e1e5a8895b04c3d33878c4be302aa1211da0967
Tyler C <TylerCode@users.noreply.github.com>
2019-11-16 01:16:07 -0700

    Too many changes, I'm a moron Should have done it incrementally

--- GDF/Assets/Monster/Scripts/Monster.cs
+++ GDF/Assets/Monster/Scripts/Monster.cs
@@ -7,23 +7,40 @@ public class Monster : MonoBehaviour
 {
     private GameObject _player;
     private NavMeshAgent _agent;
+    private LightLevel _lightLevel;
+    private GameObject _safeZone;
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
+        _safeZone = GameObject.FindGameObjectWithTag("MonsterSafeZone");
+        _lightLevel = _player.GetComponent<LightLevel>();
         _agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_agent.isStopped == true)
+        if (_lightLevel.isSafe)
         {
-            _agent.isStopped = false;
+            if (_agent.isStopped == true)
+            {
+                _agent.isStopped = false;
+                _agent.destination = _safeZone.transform.position;
+            }
+
+            _agent.destination = _safeZone.transform.position;
+        }
+        else
+        {
+            if (_agent.isStopped == true)
+            {
+                _agent.isStopped = false;
+                _agent.destination = _player.transform.position;
+            }
+
             _agent.destination = _player.transform.position;
         }
-
-        _agent.destination = _player.transform.position;
     }
 }
